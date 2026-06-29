7f4dacd40ce7e43952aa6e8c7185058836899a9b
sureshpillaibtos <psk_suresh@yahoo.com>
2026-06-29 16:56:48 +0530

    modified and  commit

--- stock_manusResearch/services/market_service.py
+++
@@ -1,7 +0,0 @@
-from services.yahoo_service import YahooService
-from config.categories import MARKET_INDICES
-
-class MarketService:
-    @staticmethod
-    def get_overview():
-        return YahooService.get_market_data(MARKET_INDICES)
